6c1871525124ae81e37265f2e931b0321d471120
Jung Choi <email": null>
2016-12-08 13:33:40 -0500

    Add youtube and omdb api calls

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,31 @@
 class MoviesController < ApplicationController
+  @youtube_search = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCi8e0iOVk1fEOogdfu4YgfA&q="
+  @omdb_title_search = "http://www.omdbapi.com/y=&plot=short&r=json&t="
+
   def index
-    
+    youtube_response = open(@youtube_search + params[:search] + youtube_token).read
+    omdb_response = open(@omdb_title_search + params[:search]).read
+
+    @movie = Movie.parse(youtube_response, omdb_response)
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+
+    else
+
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :youtube_trailer_id, :imdb_id)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,12 @@
 class Movie < ActiveRecord::Base
   validates_presence_of :title
   validates_uniqueness_of :youtube_trailer_id, :imdb_id
+
+  def self.parse(youtube_response, omdb_response)
+    youtube_hash = JSON.parse(youtube_response)
+    omdb_hash = JSON.parse(omdb_response)
+    youtube_id = youtube_hash["items"][0]["id"]["videoId"]
+
+    self.new(title: omdb_hash["Title"], youtube_trailer_id: youtube_id, imdb_id: omdb_hash["imdbID"])
+  end
 end
